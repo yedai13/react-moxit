419ff683579887f813bd18d234f87a6d2ad37854
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-17 12:09:29 -0300

    agregando h1 y button en un mismo componente

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,8 +1,13 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 
-class Counter extends React.Component {
+class Counter extends Component {
     render() { 
-        return <h1>Hello World</h1>;
+        return (
+        <React.Fragment>
+            <h1>Hello World</h1>
+            <button>Increment</button>
+        </React.Fragment>
+        );
     }
 }
  
