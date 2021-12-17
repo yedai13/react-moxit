677579c6da930e94f3c50fba15b59ae88081eb57
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-17 13:50:33 -0300

    rendering classes dynamically

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 0,
-    };
-    
-    render() { 
-        return (
-        <React.Fragment>
-            <span className='badge badge-primary m-2'>{this.formatCount()}</span>
-            <button className='btn btn-secondary btn-sm'>Increment</button>
-        </React.Fragment>
-        );
-    }
+  state = {
+    count: 0,
+  };
 
-    formatCount(){
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
