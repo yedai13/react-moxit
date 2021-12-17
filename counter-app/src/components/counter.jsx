4c426ba38ef6eede1985b0ae189abf13a97f865d
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-17 14:15:09 -0300

    conditional rendering

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -11,12 +11,21 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag."}
+        {this.renderTags()}
       </React.Fragment>
+    );
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
