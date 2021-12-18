4446ebe311cb37ed16e5badee941cfbbddd69f96
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-18 17:10:00 -0300

    composing components

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,17 +3,11 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
   };
-
-  // constructor() {
-  //   super();
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
 
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
@@ -21,9 +15,7 @@ class Counter extends Component {
         >
           Increment
         </button>
-        {this.state.tags.length === 0 && "Please create a new tag."}
-        {this.renderTags()}
-      </React.Fragment>
+      </div>
     );
   }
 
@@ -31,18 +23,6 @@ class Counter extends Component {
     console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
