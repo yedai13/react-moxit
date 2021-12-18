2281ee46e0246a1ce04049c1d6032c8d109794f6
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-18 15:53:12 -0300

    add function getMovies

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService}";
 
 class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
   render() {
     return <div></div>;
   }
