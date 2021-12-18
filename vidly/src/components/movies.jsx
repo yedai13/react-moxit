b33fc06315ffb660c81790316f30344416e4a92b
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-18 16:32:17 -0300

    add button/function delete

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -6,26 +6,42 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies: movies });
+  };
+
   render() {
     return (
-      <div>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Stock</th>
-              <th scope="col">Rate</th>
-              <th scope="col"></th>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Genre</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Rate</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
