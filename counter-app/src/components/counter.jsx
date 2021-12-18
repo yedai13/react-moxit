f5b57c172fa85c079bcab1db061dba2f4378c035
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-18 18:07:32 -0300

    updating the state, event delete

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,22 +2,22 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
   render() {
     return (
       <div>
-        {/* <h4>{this.props.id}</h4> */}
+        {/* <h4>{this.props.counter.id}</h4> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
@@ -28,7 +28,7 @@ class Counter extends Component {
 
   handleIncrement = (product) => {
     console.log(product);
-    this.setState({ count: this.state.value + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   getBadgeClasses() {
