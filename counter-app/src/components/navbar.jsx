ffe36dbe16c52799fac0b221fb79198c3811aaba
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-19 13:02:06 -0300

    lifting the state up

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,11 +1,14 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 
-class NavBar extends React.Component {
+class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
-          Navbar
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>
         </a>
       </nav>
     );
