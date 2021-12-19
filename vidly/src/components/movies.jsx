280305aa9e7534f16d50856ebd8e280f40c670b1
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-19 14:45:39 -0300

    like component

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Like from "../common/like";
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
@@ -21,6 +22,7 @@ class Movies extends Component {
               <th scope="col">Stock</th>
               <th scope="col">Rate</th>
               <th scope="col"></th>
+              <th scope="col"></th>
             </tr>
           </thead>
           <tbody>
@@ -30,6 +32,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
@@ -49,6 +57,14 @@ class Movies extends Component {
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies: movies });
+  };
+
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
   };
 }
 
