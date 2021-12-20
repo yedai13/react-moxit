fbe21b21de3429cfbe1b6cd432dac6d19d3e07bc
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-20 14:47:27 -0300

    sorting raising the sort event

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -15,7 +15,7 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    const genres = [{ name: "All Genres" }, ...getGenres()];
+    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
     this.setState({ movies: getMovies(), genres });
   }
 
@@ -52,6 +52,7 @@ class Movies extends Component {
             movies={movies}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
+            onSort={this.handleSort}
           />
           <Pagination
             itemsCount={filtered.length}
@@ -83,6 +84,10 @@ class Movies extends Component {
 
   handleGenreSelect = (genre) => {
     this.setState({ selectedGenre: genre, currentPage: 1 });
+  };
+
+  handleSort = (path) => {
+    console.log(path);
   };
 }
 
--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import Like from "./common/like";
 
 const MoviesTable = (props) => {
-  const { movies, onDelete, onLike } = props;
+  const { movies, onDelete, onLike, onSort } = props;
   return (
     <table className="table">
       <thead>
         <tr>
-          <th scope="col">Title</th>
-          <th scope="col">Genre</th>
-          <th scope="col">Stock</th>
-          <th scope="col">Rate</th>
+          <th onClick={() => onSort("title")} scope="col">
+            Title
+          </th>
+          <th onClick={() => onSort("genre.name")} scope="col">
+            Genre
+          </th>
+          <th onClick={() => onSort("numberInStock")} scope="col">
+            Stock
+          </th>
+          <th onClick={() => onSort("dailyRentalRate")} scope="col">
+            Rate
+          </th>
           <th scope="col"></th>
           <th scope="col"></th>
         </tr>
