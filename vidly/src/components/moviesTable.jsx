4113ff247a17a5c02b2a262b7778dc8d8f1d2e98
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-20 15:56:03 -0300

    sorting extracting TableBody

--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import TableHeader from "./common/tableHeader";
 import Like from "./common/like";
+import TableBody from "./common/tableBody";
 
 class MoviesTable extends Component {
   columns = [
@@ -21,6 +22,8 @@ class MoviesTable extends Component {
           sortColumn={sortColumn}
           onSort={onSort}
         />
+
+        <TableBody data={movies} />
         <tbody>
           {movies.map((movie) => (
             <tr key={movie._id}>
