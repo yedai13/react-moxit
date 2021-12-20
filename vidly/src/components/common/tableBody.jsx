a17628d426a5dcf08c1b4cfff594e0c0afc73beb
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-20 16:21:30 -0300

    sorting unique keys final

--- vidly/src/components/common/tableBody.jsx
+++ vidly/src/components/common/tableBody.jsx
@@ -7,9 +7,11 @@ class TableBody extends Component {
     return (
       <tbody>
         {data.map((item) => (
-          <tr>
+          <tr key={item._id}>
             {columns.map((column) => (
-              <td>{this.renderCell(item, column)}</td>
+              <td key={this.createKey(item, column)}>
+                {this.renderCell(item, column)}
+              </td>
             ))}
           </tr>
         ))}
@@ -21,6 +23,10 @@ class TableBody extends Component {
     if (column.content) return column.content(item);
 
     return _.get(item, column.path);
+  };
+
+  createKey = (item, column) => {
+    return item._id + (column.path || column.key);
   };
 }
 
