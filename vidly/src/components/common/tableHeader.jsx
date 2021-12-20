055b390abc65d32bf02a9091d77a606fc28752dc
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-20 15:41:44 -0300

    sorting extracting TableHeader

---
+++ vidly/src/components/common/tableHeader.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class TableHeader extends React.Component {
+  render() {
+    return (
+      <thead>
+        <tr>
+          {this.props.columns.map((column) => (
+            <th
+              key={column.path || column.key}
+              onClick={() => this.raiseSort(column.path)}
+              scope="col"
+            >
+              {column.label}
+            </th>
+          ))}
+        </tr>
+      </thead>
+    );
+  }
+
+  raiseSort = (path) => {
+    const sortColumn = { ...this.props.sortColumn };
+    if (sortColumn.path === path)
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    else {
+      sortColumn.path = path;
+      sortColumn.order = "asc";
+    }
+    this.props.onSort(sortColumn);
+  };
+}
+
+export default TableHeader;
