cc445aa18b108e0220ac04bc4e45440f71e4d0c3
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-22 12:43:00 -0300

    register form

---
+++ vidly/src/components/registerForm.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+
+class RegisterForm extends Form {
+  state = {
+    data: { username: "", password: "", name: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().email().required().label("Username"),
+    password: Joi.string().min(5).required().label("Password"),
+    name: Joi.string().required().label("Name"),
+  };
+
+  doSubmit = () => {
+    //call the server
+    console.log("submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Resgister</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
