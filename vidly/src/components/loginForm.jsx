842d66b97b09453a8a187b4fe59512b4188e9f98
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-22 10:54:06 -0300

    joi

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -6,9 +7,18 @@ class LoginForm extends Component {
     account: { username: "", password: "" },
     errors: {},
   };
-  username = React.createRef();
+
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  };
 
   validate = () => {
+    const result = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false,
+    });
+    console.log(result);
+
     const errors = {};
 
     const { account } = this.state;
