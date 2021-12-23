0c66f3c164e46d6e1c8f92dadca329e1758ef721
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-23 12:28:05 -0300

    extracting a logger service

--- http-app/src/index.js
+++ http-app/src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import logger from "./services/logService";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-Sentry.init({
-  dsn: "https://83f05641bb0d4e988818e28478db8326@o1097819.ingest.sentry.io/6119592",
-  integrations: [new Integrations.BrowserTracing()],
-  tracesSampleRate: 1.0,
-});
+logger.init();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
---
+++ http-app/src/services/logService.js
@@ -0,0 +1,19 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn: "https://83f05641bb0d4e988818e28478db8326@o1097819.ingest.sentry.io/6119592",
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+export default {
+  init,
+  log,
+};
