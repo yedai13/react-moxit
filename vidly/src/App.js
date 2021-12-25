c24fe48af53ca3fb1dc8d4be22184567eb97f42a
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-25 15:25:02 -0300

    loggin out a user

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -9,6 +9,7 @@ import NotFound from "./components/notFound";
 import NavBar from "./components/navbar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
+import Logout from "./components/common/logout";
 import RegisterForm from "./components/registerForm";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
@@ -33,6 +34,7 @@ class App extends Component {
           <Switch>
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
