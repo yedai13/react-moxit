d58c6e8fd6ad15556764302506cab3a294941dae
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-27 11:47:59 -0300

    the useEffect Hook

--- react-advanced/src/hooks/Counter.jsx
+++ react-advanced/src/hooks/Counter.jsx
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Counter(props) {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
+
+  useEffect(() => {
+    document.title = `${name} has clicked ${count} times!`;
+
+    return () => {
+      console.log("Clean up");
+      //componentWillUnmount
+    };
+  }, [count]);
+
   return (
     <React.Fragment>
       <input type="text" onChange={(e) => setName(e.target.value)}></input>
